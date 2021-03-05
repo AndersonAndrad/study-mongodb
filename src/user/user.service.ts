8f0f9cc4538b665afc824fc31826efcff12864c5
AndersonAndrad <anderson_andrade_@outlook.com>
2021-03-04 21:26:41 -0300

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,41 +4,39 @@ import { UserEntity } from './user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
-    private userRepository: Repository<UserEntity>,
+    private _userRepository: Repository<UserEntity>,
   ) {}
 
   async read() {
-    return await this.userRepository.find();
+    return await this._userRepository.find();
   }
 
   async readOnlyOne(id: string) {
-    return await this.userRepository.findOne(id);
+    return await this._userRepository.findOne(id);
   }
 
   async create(data: IUser) {
-    const user = this.userRepository.create(data);
+    const user = this._userRepository.create(data);
     const error = await validate(user);
 
     if (error.length != 0) {
       return error;
     }
-
-    return await this.userRepository.save(user);
+    return await this._userRepository.save(user);
   }
 
   async update(id: string, data: IUser) {
-    await this.userRepository.update(id, data);
-    return await this.userRepository.findOne(id);
+    await this._userRepository.update(id, data);
+    return await this._userRepository.findOne(id);
   }
 
   async delete(id: string) {
-    const user = await this.userRepository.findOne(id);
-    await this.userRepository.delete(id);
+    const user = await this._userRepository.findOne(id);
+    await this._userRepository.delete(id);
     return { user, deleted: true };
   }
 }
